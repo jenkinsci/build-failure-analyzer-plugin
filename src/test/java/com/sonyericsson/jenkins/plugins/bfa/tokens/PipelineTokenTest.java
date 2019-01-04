a2d10a229db53f9e692624a68011e63bcd03ee89
Michael Broadbent <michael.broadbent@amilia.com>
2019-01-04 13:37:31 -0500

    scripted pipeline test and checkstyle fixes

--- src/test/java/com/sonyericsson/jenkins/plugins/bfa/tokens/PipelineTokenTest.java
+++ src/test/java/com/sonyericsson/jenkins/plugins/bfa/tokens/PipelineTokenTest.java
@@ -4,10 +4,8 @@ import hudson.FilePath;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.junit.Assert.assertEquals;
@@ -16,33 +14,43 @@ import static org.junit.Assert.assertEquals;
  * Tests that the plugin is compatible with the token macro pipeline step.
  */
 public class PipelineTokenTest {
+    /**
+     * The Jenkins Rule.
+     */
     @Rule
+    //CS IGNORE VisibilityModifier FOR NEXT 1 LINES. REASON: Jenkins Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
 
-    @ClassRule
-    public static BuildWatcher buildWatcher = new BuildWatcher();
+    //CS IGNORE LineLength FOR NEXT 11 LINES. REASON: Test data.
+    private static final String DECLARATIVE_PIPELINE =
+                      "pipeline {\n"
+                    + "  agent any\n"
+                    + "  stages {\n"
+                    + "    stage(\"Run declarative bfa\") {\n"
+                    + "      steps {\n"
+                    + "        writeFile file: 'bfa.log', text: tm('''${BUILD_FAILURE_ANALYZER, noFailureText=\"No errors found - Declarative\"}''')\n"
+                    + "      }\n"
+                    + "    }\n"
+                    + "  }\n"
+                    + "}\n";
 
-    private static final String BUILD_SCRIPT =
-  "pipeline {\n"
-+ "    agent any\n"
-+ "    stages {\n"
-+ "        stage(\"Run bfa\") {\n"
-+ "            steps {\n"
-+ "                writeFile file: 'bfa.log', text: tm('''${BUILD_FAILURE_ANALYZER, noFailureText=\"None Found\"}''')\n"
-+ "            }\n"
-+ "        }\n"
-+ "    }\n"
-+ "}\n";
+    //CS IGNORE LineLength FOR NEXT 6 LINES. REASON: Test data.
+    private static final String SCRIPTED_PIPELINE =
+                      "node {\n"
+                    + "  stage(\"Run scripted bfa\") {\n"
+                    + "    writeFile file: 'bfa.log', text: tm('''${BUILD_FAILURE_ANALYZER, noFailureText=\"No errors found - Scripted\"}''')\n"
+                    + "  }\n"
+                    + "}\n";
 
     /**
-     * Tests that the plugin is run by the tm pipeline step and writes the result to a file.
+     * Tests that the plugin is run by the tm pipeline step in a declarative pipeline by writing the result to a file.
      *
      * @throws Exception If necessary
      */
     @Test
-    public void pipelineEcho() throws Exception {
+    public void declarativePipelineTokenMacro() throws Exception {
         WorkflowJob project = jenkinsRule.createProject(WorkflowJob.class);
-        project.setDefinition(new CpsFlowDefinition(BUILD_SCRIPT, true));
+        project.setDefinition(new CpsFlowDefinition(DECLARATIVE_PIPELINE, true));
 
         final WorkflowRun build = jenkinsRule.buildAndAssertSuccess(project);
         final FilePath workspace = jenkinsRule.jenkins.getWorkspaceFor(project);
@@ -50,6 +58,25 @@ public class PipelineTokenTest {
 
         final String bfaLogText = bfaLog.readToString();
 
-        assertEquals("None Found", bfaLogText);
+        assertEquals("No errors found - Declarative", bfaLogText);
+    }
+
+    /**
+     * Tests that the plugin is run by the tm pipeline step in a scripted pipeline by writing the result to a file.
+     *
+     * @throws Exception If necessary
+     */
+    @Test
+    public void scriptedPipelineTokenMacro() throws Exception {
+        WorkflowJob project = jenkinsRule.createProject(WorkflowJob.class);
+        project.setDefinition(new CpsFlowDefinition(SCRIPTED_PIPELINE, true));
+
+        final WorkflowRun build = jenkinsRule.buildAndAssertSuccess(project);
+        final FilePath workspace = jenkinsRule.jenkins.getWorkspaceFor(project);
+        final FilePath bfaLog = workspace.child("bfa.log");
+
+        final String bfaLogText = bfaLog.readToString();
+
+        assertEquals("No errors found - Scripted", bfaLogText);
     }
 }
