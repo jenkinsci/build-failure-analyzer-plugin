742e1e484c990bdbabfa71ff8d883f2cc3e2cd42
Tim Jacomb <timjacomb1+github@gmail.com>
2019-06-20 21:15:11 +0100

    Fix classloader issue
    It wasn't possible to deserialise mongo classes with out this change,
    for details see:
    https://github.com/jenkinsci/build-failure-analyzer-plugin/pull/97#issuecomment-504148433
    and
    https://jenkins.io/doc/developer/plugin-development/dependencies-and-class-loading/

--- src/main/java/com/sonyericsson/jenkins/plugins/bfa/db/MongoDBKnowledgeBase.java
+++ src/main/java/com/sonyericsson/jenkins/plugins/bfa/db/MongoDBKnowledgeBase.java
@@ -23,6 +23,8 @@
  */
 package com.sonyericsson.jenkins.plugins.bfa.db;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.mongodb.AggregationOutput;
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
@@ -50,19 +52,6 @@ import hudson.model.Descriptor;
 import hudson.model.Run;
 import hudson.util.FormValidation;
 import hudson.util.Secret;
-import jenkins.model.Jenkins;
-import org.apache.commons.collections.keyvalue.MultiKey;
-import org.bson.types.ObjectId;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.Hour;
-import org.jfree.data.time.Month;
-import org.jfree.data.time.TimePeriod;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-import org.mongojack.DBCursor;
-import org.mongojack.JacksonDBCollection;
-import org.mongojack.WriteResult;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
@@ -80,6 +69,19 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import jenkins.model.Jenkins;
+import org.apache.commons.collections.keyvalue.MultiKey;
+import org.bson.types.ObjectId;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.Hour;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimePeriod;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.mongojack.DBCursor;
+import org.mongojack.JacksonDBCollection;
+import org.mongojack.WriteResult;
+import org.mongojack.internal.MongoJackModule;
 
 import static java.util.Arrays.asList;
 
@@ -103,6 +105,13 @@ public class MongoDBKnowledgeBase extends KnowledgeBase {
     private static final Logger logger = Logger.getLogger(MongoDBKnowledgeBase.class.getName());
     private static final int CONNECT_TIMEOUT = 5000;
     private static final int SERVER_SELECTION_TIMEOUT = 5000;
+
+    private static final TypeFactory TYPE_FACTORY = TypeFactory.defaultInstance()
+            .withClassLoader(MongoDBKnowledgeBase.class.getClassLoader());
+
+    private static final ObjectMapper OBJECT_MAPPER = MongoJackModule
+            .configure(new ObjectMapper())
+            .setTypeFactory(TYPE_FACTORY);
 
     private transient MongoClient mongo;
     private transient DB db;
@@ -1087,7 +1096,7 @@ public class MongoDBKnowledgeBase extends KnowledgeBase {
             if (collection == null) {
                 collection = getCollection();
             }
-            jacksonCollection = JacksonDBCollection.wrap(collection, FailureCause.class, String.class);
+            jacksonCollection = JacksonDBCollection.wrap(collection, FailureCause.class, String.class, OBJECT_MAPPER);
         }
         return jacksonCollection;
     }
@@ -1101,7 +1110,8 @@ public class MongoDBKnowledgeBase extends KnowledgeBase {
             if (statisticsCollection == null) {
                 statisticsCollection = getStatisticsCollection();
             }
-            jacksonStatisticsCollection = JacksonDBCollection.wrap(statisticsCollection, Statistics.class, String.class);
+            jacksonStatisticsCollection = JacksonDBCollection.wrap(statisticsCollection, Statistics.class, String.class,
+                    OBJECT_MAPPER);
         }
         return jacksonStatisticsCollection;
     }
